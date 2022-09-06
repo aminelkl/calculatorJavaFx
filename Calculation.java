581d294c032c1079b6b188c6706ba98ed99e845a
Amine Lakhal <96929412+aminelkl@users.noreply.github.com>
2022-09-05 23:37:24 -0400

    Update Calculation.java

--- Calculation.java
+++ Calculation.java
@@ -1,32 +1,24 @@
 public class Calculation {
 
-	public float calculate(float num1, float num2, String operator) 
+	public long calculate(long num1, long num2, String operator) 
 	{
 		switch (operator) 
 		{
-			case "+" :
-			{
-			return num1+num2;
-			}
-			case "-" :
-			{
+		case "+":
+		    return num1+num2;
+		case "-":
 			return num1-num2;
-			}
-			case "*" :
-			{
+		case "x":
 			return num1*num2;
-			}
-			case "/" :
-			{
-				if (num2 == 0)
-				{
-				    return 0;
-				}
-				return num1/num2;
-			}
-			default : 
+		case "/":
+			if(num2==0)
 				return 0;
+			return num1/num2;
+		default:
+			return 0 ;
 		}
-	}
+	
+}
+
 	
 }
